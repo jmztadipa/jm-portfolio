bd2a709e017e0b0160ffcae838590f2dfbc3bdac
smpctadipa <jztadipa@smcdacon.com>
2025-09-07 18:30:34 +0800

    Fixed main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,10 +8,9 @@ import App from './App.jsx'
 // CSS
 import './index.css'
 
-/*
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <App />
+    <App />
   </StrictMode>,
-);
-*/
+)
+
